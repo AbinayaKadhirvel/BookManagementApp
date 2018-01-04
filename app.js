ea9ba469bea700a83c19ede35092432bc39eb593
Yang Lu <y80053445@china.huawei.com>
2018-01-04 11:33:36 -0800

    demo example: simple routing
    Module 2.4

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var app = express();
 
 var port = 5000;
 
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.get('/books', function(req, res){
+    res.send('Hello Books');
+});
+
 app.listen(port, function(err){
     if(err){
         console.log(err);
