8979ad8604fc80dc0e94459ef669f1d3412ecfb3
Yang Lu <y80053445@china.huawei.com>
2018-01-04 11:19:52 -0800

    demo example: npm start
    Module 2.4

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 var express = require('express');
+
+// To create an instance of express
+var app = express();
+
+var port = 5000;
+
+app.listen(port, function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log('Running server on port: ' + port);
+    }
+});
