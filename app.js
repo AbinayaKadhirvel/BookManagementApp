386b4617f7a80784c9ef1b514ae1c11c83469e90
Leon <yanglyu.leon.7@gmail.com>
2018-05-14 21:16:17 -0700

    demo: Static Files - Public Directory
    Module 3.6

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
 var morgan = require('morgan');
+var path = require('path');
 // To create an instance of express
 var app = express();
 
 var port = 5000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
