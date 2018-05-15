6cbf827ca1839f8e689cbce0a4c997984772f4ae
Leon <yanglyu.leon.7@gmail.com>
2018-05-15 12:13:00 -0700

    demo: Setting upo Nodemon
    Module 4.8

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 // To create an instance of express
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(morgan('combined'));
 
