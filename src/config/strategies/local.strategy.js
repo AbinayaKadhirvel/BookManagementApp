50f0eb1c1978e596e3b7cc4bf668c7ebafee39a4
Karpagambigai <36296018+Karpagambigai@users.noreply.github.com>
2018-05-24 23:25:40 +0530

    Add files via upload

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,1 +1,26 @@
+const passport = require('passport');
+const debug = require('debug')('app:local.strategy');
+const { Strategy } = require('passport-local');
+const mongoose = require('mongoose');
 
+const db = mongoose.connect('mongodb://localhost/libraryApp');
+const User = require('../../../models/libraryUserModel.js');
+
+module.exports = function localStrategy() {
+    passport.use(new Strategy({
+        usernameField: 'username',
+        passwordField: 'password'
+    }, (username, password, done) => {
+      const query = {};
+      query.email = username;
+      debug(username);
+      User.findOne(query, (err, user) => {
+          if (user && user.password === password) {
+            done(null, user);
+          } 
+          else {
+            done(null, false);
+          }
+        });
+    }));  
+};
